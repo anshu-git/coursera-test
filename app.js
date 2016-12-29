51778838a8a5715d5fb3c41d73ade1e96709ed6b
Anshuman <anshuman21@live.in>
2016-12-29 19:22:35 +0530

    Assignment1-final changes

---
+++ app.js
@@ -0,0 +1,22 @@
+(function () {
+'use strict';
+
+angular.module('assignment1', [])
+
+.controller('LunchCheckController', function ($scope) {
+$scope.lunch_menu ="";
+$scope.checkDiet = function () {
+  var items=$scope.lunch_menu.split(',');
+  console.log(items);
+  if (items.length==1 && items[0]==='') {
+    $scope.message="Please enter data first";
+  } else if(items.length<=3) {
+      $scope.message="Enjoy!";
+  }
+  else {
+    $scope.message="Too much!"
+  }
+};
+});
+
+})();
